ecb8214be194718a44e4bd4617dc22e42c3e3431
知嘻晓 <2351813972@qq.com>
2020-08-10 21:34:10 +0800

    调整路由

--- src/router/index.js
+++ src/router/index.js
@@ -56,106 +56,79 @@ export const constantRoutes = [
   },
 
   {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
+    path: '/users',
     component: Layout,
     children: [
       {
         path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
+        name: 'Users',
+        component: () => import('@/views/users/index'),
+        meta: { title: '用户中心', icon: 'user'}
       }
     ]
   },
 
   {
-    path: '/nested',
+    path: '/goods',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
+        path: 'index',
+        name: 'Goods',
+        component: () => import('@/views/goods/index'),
+        meta: { title: '商品中心', icon: 'goods'}
       }
     ]
   },
 
   {
-    path: 'external-link',
+    path: '/order',
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
+        path: 'index',
+        name: 'Order',
+        component: () => import('@/views/order/index'),
+        meta: { title: '订单中心', icon: 'order'}
+      }
+    ]
+  },
+
+  {
+    path: '/credit',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Credit',
+        component: () => import('@/views/credit/index'),
+        meta: { title: '积分中心', icon: 'credit'}
+      }
+    ]
+  },
+
+  {
+    path: '/logistical',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Logistical',
+        component: () => import('@/views/logistical/index'),
+        meta: { title: '物流中心', icon: 'logistical'}
+      }
+    ]
+  },
+
+  {
+    path: '/ad',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Ad',
+        component: () => import('@/views/ad/index'),
+        meta: { title: '广告中心', icon: 'ad'}
       }
     ]
   },
---
+++ src/views/logistical/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>物流中心</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
